17526dcad41ab54892fac1da61deaec6d33d0019
Bart Calixto <bart@bartmax.com>
2015-06-03 05:12:51 -0300

    removed Uri.Schemehttps in favor of constant string "https" and upper bound check of 65535 as suggestion from david fowler and JamesH.

--- src/CustomPortHttps/CustomPortHttpsMiddleware.cs
+++ src/CustomPortHttps/CustomPortHttpsMiddleware.cs
@@ -10,9 +10,10 @@ namespace CustomPortHttps
     {
         private readonly RequestDelegate _next;
         private readonly int _port;
+        private const string SCHEME_HTTPS = "https";
         public CustomPortHttpsMiddleware(RequestDelegate next, int port)
         {
-            if (!(port > 0)) throw new ArgumentOutOfRangeException(nameof(port), "Port must be greater than 0");
+            if (!(port > 0 && port < 65535)) throw new ArgumentOutOfRangeException(nameof(port), "Port must be greater than 0 and less than 65535");
             _next = next;
             _port = port;
         }
@@ -23,12 +24,7 @@ namespace CustomPortHttps
             if (context.Response.StatusCode == (int)HttpStatusCode.MovedPermanently)
             {
                 var location = context.Response.GetTypedHeaders().Location;
-#if DNX451
-                string schemeHttps = Uri.UriSchemeHttps;
-#else
-                string schemeHttps = "https";
-#endif
-                if (String.Equals(location.Scheme, schemeHttps, StringComparison.OrdinalIgnoreCase))
+                if (String.Equals(location.Scheme, SCHEME_HTTPS, StringComparison.OrdinalIgnoreCase))
                 {
                     var builder = new UriBuilder(location);
                     builder.Port = _port;
